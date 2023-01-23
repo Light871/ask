06f0985137626a22d84bfa7f35befa92395d91b9
Light <light@MacBook-Pro-Light.local>
2023-01-23 21:12:32 +0700

    Add user, sessions and links at navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :require_no_authentication, only: %i[new create]
+  before_action :require_authentication, only: %i[edit update]
+  before_action :set_user!, only: %i[edit update]
+
   def new
     @user = User.new
   end
@@ -7,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = @user.id
+      sign_in(@user)
       flash[:success] = "Welcome to the app, #{current_user.name_or_email}!"
       redirect_to root_path
     else
@@ -15,7 +19,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      sign_in(@user)
+      flash[:success] = "Profile updated successfully!"
+      redirect_to edit_user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_user!
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :name, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'questions#index'
+
+  resource :session, only: %i[new create destroy]
   
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update]
 
   resources :questions do
     resources :answers, except: %i[new show]
