c02ffc49b8f189e4e3f752984c4e0ffbcaa484bf
Light <light@MacBook-Pro-Light.local>
2023-02-02 22:17:39 +0700

    Add TomSelect and ajax search

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,6 @@
 class QuestionsController < ApplicationController
   include QuestionsAnswers
   before_action :set_question!, only: %i[show destroy edit update]
-  before_action :fetch_tags, only: %i[new edit]
 
   def index
     @pagy, @questions = pagy Question.all_by_tags(params[:tag_ids])
@@ -53,9 +52,5 @@ class QuestionsController < ApplicationController
 
   def set_question!
     @question = Question.find params[:id]
-  end
-
-  def fetch_tags
-    @tags = Tag.all
   end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,6 +6,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import '@popperjs/core'
 import 'bootstrap/js/dist/dropdown'
 import 'bootstrap/js/dist/collapse'
+import './scripts/select'
 
 Rails.start()
 Turbolinks.start()
