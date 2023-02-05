0ac8f834aed25a48cc273e74d1f63034a24e1b0b
Light <light@MacBook-Pro-Light.local>
2023-02-06 01:40:35 +0700

    Add sidekiq and transfer mailers and users import/export to background

---
+++ app/mailers/admin/user_mailer.rb
@@ -0,0 +1,24 @@
+module Admin
+  class UserMailer < ApplicationMailer
+    def bulk_import_done
+      @user = params[:user]
+
+      mail to: @user.email, subject: 'Import done successfully!'
+    end
+
+    def bulk_import_fail
+      @error = params[:error]
+      @user = params[:user]
+
+      mail to: @user.email, subject: 'Import failed!!'
+    end
+
+    def bulk_export_done
+      @user = params[:user]
+      zipped_blob = params[:zipped_blob]
+
+      attachments[zipped_blob.attachable_filename] = zipped_blob.download
+      mail to: @user.email, subject: 'Export done successfully!'
+    end
+  end
+end
